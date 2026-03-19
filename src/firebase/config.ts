fe76956c04152f67ef62e661810ab99d2549da14
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-19 11:12:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7922254713-4e326",
+  "appId": "1:253130465197:web:09f84ae6f3d76234034b24",
+  "apiKey": "AIzaSyBmwxxsAlunkZZwPO9szWfS51dNmKKZmho",
+  "authDomain": "studio-7922254713-4e326.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "253130465197"
+};
