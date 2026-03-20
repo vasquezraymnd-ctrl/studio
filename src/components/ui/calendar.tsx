d83a42d66689a874b1fb40751fc48393f2d874c8
Raymond Vasquez <vasquez.raymnd@gmail.com>
2026-03-20 02:52:51 +0000

    fina?

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,10 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        iconLeft: ({ className, ...props }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        iconRight: ({ className, ...props }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
